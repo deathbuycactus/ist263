a136acc19d5b9d5ee33619bb82561a5d5ef46469
deathbuycactus <mjohns41@syr.edu>
2024-12-02 12:19:55 -0500

    p7

---
+++ project7/p5_scripts.js
@@ -0,0 +1,7 @@
+function change() {
+    /* Give id='gridding' to zuko and iroh image */
+    document.getElementById("gridding").setAttribute("src", "../final_project_images/zuko_iroh_gridded.png")
+}
+function revert() {
+    document.getElementById("gridding").setAttribute("src", "../final_project_images/zuko_iroh.png")
+}
